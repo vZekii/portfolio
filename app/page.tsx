550a3a23ecc8c2ad21db21007dd1a5488ea01a02
Zekii <zach7505@gmail.com>
2024-06-14 00:45:38 +1000

    Added scrollspy for each section, and dynamic nav updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 'use client'
-import Image from "next/image";
-import { CubeIcon } from "@heroicons/react/24/outline";
 import Navbar from "@/components/navbar";
 import ProjectCard from "@/components/ProjectCard";
+import { ArrowUpIcon } from "@heroicons/react/24/outline";
 import { useRef } from 'react';
 
 
 export default function Home() {
-
-  const projectsRef = useRef(null);
+  
+  const homeRef = useRef<HTMLDivElement | null>(null);
+  const projectsRef = useRef<HTMLDivElement | null>(null);
+  const blogRef = useRef<HTMLDivElement | null>(null);
+  const contactRef = useRef<HTMLDivElement | null>(null);
 
   const projects = [
     {
@@ -40,13 +42,16 @@ export default function Home() {
 
   return (
     <div className="mt-[--header-height] h-screen max-w-7xl m-auto">
-      <Navbar projectRef={projectsRef} />
-      <div className="p-4 lg:p-10 border-b border-black border-dashed dark:border-white">
+      <Navbar homeRef={homeRef} projectRef={projectsRef} blogRef={blogRef} contactRef={contactRef}/>
+      <div ref={homeRef} className="p-4 lg:p-10 border-b border-black border-dashed dark:border-white">
         <h1 className="dark:text-white text-4xl md:text-5xl lg:text-7xl pb-16 pt-16 text-center font-mono ">Hey, I'm Zach!</h1>
         <h2 className="dark:text-white text-1xl md:text-3xl lg:text-4xl pb-10 text-center font-mono">I'm a software developer, with a speciality in quantum computing</h2>
 
         <div className="justify-center items-center flex mt-5 pb-20">
-          <button className="mx-2 px-4 py-1 lg:text-lg rounded-full border-2 border-black dark:border-white font-mono dark:text-white hover:bg-black hover:dark:bg-white hover:text-white hover:dark:text-slate-800">
+          <button className="mx-2 px-4 py-1 lg:text-lg rounded-full border-2 border-black dark:border-white font-mono dark:text-white hover:bg-black hover:dark:bg-white hover:text-white hover:dark:text-slate-800"
+            onClick={() => {contactRef.current?.scrollIntoView({
+              behavior: 'smooth',
+            }); }}>
             Contact
           </button>
           <button className="mx-2 px-4 py-1 lg:text-lg rounded-full border-2 border-black dark:border-white font-mono dark:text-white hover:bg-black hover:dark:bg-white hover:text-white hover:dark:text-slate-800">
@@ -57,30 +62,44 @@ export default function Home() {
         {/* <p className="dark:text-white text-l font-mono text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam, quam id tincidunt tincidunt, nunc nisl ultrices nunc, id lacinia nunc nunc id velit. Nulla facilisi. Sed auctor, nunc id aliquet luctus, nisl nunc tincidunt nunc, id aliquam nunc nunc ac nunc. Sed euismod, nunc id aliquam aliquam, nunc nunc aliquam nunc, id aliquam nunc nunc ac nunc. Sed euismod, nunc id aliquam aliquam, nunc nunc aliquam nunc, id aliquam nunc nunc ac nunc. Sed euismod, nunc id aliquam aliquam, nunc nunc aliquam nunc, id aliquam nunc nunc ac nunc. Sed euismod, nunc id aliquam aliquam, nunc nunc aliquam nunc, id aliquam nunc nunc ac nunc. Sed euismod, nunc id aliquam aliquam, nunc nunc aliquam nunc, id aliquam nunc nunc ac nunc.</p> */}
       </div>
 
-      <div ref={projectsRef} className="p-4 lg:p-10 scroll-m-10">
-        <h1 className="text-3xl font-mono pb-5 text-center dark:text-white">Projects</h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 justify-center">
+      <div ref={projectsRef} id="projects" className="p-4 lg:p-10 scroll-m-10">
+        <h1 className="text-3xl font-mono pb-5 text-center dark:text-white">
+          <a href="#Projects">Projects</a>
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 justify-center">
           {projects.map((project) => (
             <ProjectCard key={project.id} project={project} />
           ))}
         </div>
       </div>
-      <div className="p-4 lg:p-10">
-        <h1 className="text-3xl font-mono pb-5 text-center dark:text-white">Projects</h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 justify-center">
-          {projects.map((project) => (
-            <ProjectCard key={project.id} project={project} />
-          ))}
-        </div>
+      
+
+      <div ref={blogRef} id="blog" className="p-4 lg:p-10 scroll-m-10 h-screen">
+        <h1 className="text-3xl font-mono pb-5 text-center dark:text-white">
+          <a href="#Blog">Blog</a>
+        </h1>
       </div>
-      <div className="p-4 lg:p-10">
-        <h1 className="text-3xl font-mono pb-5 text-center dark:text-white">Projects</h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 justify-center">
-          {projects.map((project) => (
-            <ProjectCard key={project.id} project={project} />
-          ))}
-        </div>
+      
+
+      <div ref={contactRef} id="contact" className="p-4 lg:p-10 scroll-m-10 h-screen">
+        <h1 className="text-3xl font-mono pb-5 text-center dark:text-white">
+          <a href="#Contact">Contact</a>
+        </h1>
+          
       </div>
+    
+    
+
+      <footer className="bg-black ">
+        <button onClick={() => {
+          window.scroll({
+            top: 0,
+            behavior: 'smooth'
+          });
+        }}>
+          <ArrowUpIcon className="size-5" aria-label="to top"/>
+        </button>
+      </footer>
     </div>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { CubeIcon, SunIcon, MoonIcon, Bars3Icon } from '@heroicons/react/24/outline';
 
-export default function Navbar({ projectRef }: { projectRef: React.RefObject<any> }) {
+export default function Navbar({ homeRef, projectRef, blogRef, contactRef  }: { homeRef: React.RefObject<any>, projectRef: React.RefObject<any>, blogRef: React.RefObject<any>, contactRef: React.RefObject<any> }) {
     const [dark, setDark] = React.useState(false);
+    const [activeSection, setActiveSection] = useState('home');
+
+    const navItems = [
+        {
+            name: 'Home',
+            ref: homeRef
+        },
+        {
+            name: 'Projects',
+            ref: projectRef
+        },
+        {
+            name: 'Blog',
+            ref: blogRef
+        },
+        {
+            name: 'Contact',
+            ref: contactRef
+        }
+    ];
 
     const darkModeHandler = () => {
         setDark(!dark);
@@ -31,26 +51,40 @@ export default function Navbar({ projectRef }: { projectRef: React.RefObject<any
         });
       }, []);
 
+    useEffect (() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            if (scrollPosition < 100) {
+                setActiveSection('home');
+            } else if (scrollPosition >= projectRef.current?.offsetTop - 100 && scrollPosition < blogRef.current?.offsetTop - 100) {
+                setActiveSection('projects');
+            } else if (scrollPosition >= blogRef.current?.offsetTop - 100 && scrollPosition < contactRef.current.offsetTop - 100) {
+                setActiveSection('blog');
+            } else if (scrollPosition >= contactRef.current.offsetTop - 100 ) {
+                setActiveSection('contact');
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [activeSection]);
+
 
     return (
-        <header className="h-[--header-height] fixed top-0 left-0 right-0 border-b border-gray-200 bg-slate-50 dark:bg-slate-800 dark:border-slate-600">
+        <header className="h-[--header-height] fixed top-0 left-0 right-0 border-b border-gray-200 bg-slate-50 dark:bg-slate-800 dark:border-slate-700">
             <div className='flex self-start h-full absolute align-middle items-center pl-2'>
                 <CubeIcon className="dark:text-white size-5" />
                 <p className='align-middle pl-2 dark:text-white font-mono'>Zach Clare</p>
             </div>
 
             <nav className='align-middle justify-center items-center h-full space-x-1 hidden md:flex font-mono'>
-                <a href="/" className='font-bold text-sm bg-black dark:bg-white text-white dark:text-black rounded-full px-3 py-1'>Home</a>
-                <a href="/" className='font-light text-sm rounded-full px-3 py-1 hover:bg-gray-200 dark:hover:bg-gray-700 dark:text-white'
-                    onClick={() => {projectRef.current.scrollIntoView({
-                        behavior: 'smooth',
-                        block: 'nearest',
-                        inline: 'center'
-                      }); }}>Projects</a>
-                <a href="/" className='font-light text-sm rounded-full px-3 py-1 hover:bg-gray-200 dark:hover:bg-gray-700 dark:text-white'>Blog</a>
-                <a href="/" className='font-light text-sm rounded-full px-3 py-1 hover:bg-gray-200 dark:hover:bg-gray-700 dark:text-white'>Contact</a>
+                {navItems.map((navitem) => (
+                    <a key={navitem.name} href={`#${navitem.name}`} className={`text-sm rounded-full px-3 py-1 ${activeSection === navitem.name.toLowerCase() ? 'font-bold bg-black dark:bg-white text-white dark:text-black' : 'font-light hover:bg-gray-200 dark:hover:bg-gray-700 dark:text-white'}`}
+                        onClick={() => {navitem.ref.current?.scrollIntoView({
+                            behavior: 'smooth',
+                          }); }}>{navitem.name}</a>
+                ))}
             </nav>
-
             <div className='flex align-middle items-center h-full absolute top-0 right-0 pr-2'>
                 <button className='p-2 rounded-md hover:bg-gray-200 dark:hover:bg-gray-700' onClick={() => darkModeHandler()}>
                     {
