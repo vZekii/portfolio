f3e22937b25262f7701b5407444d9e5889b0e44d
Zekii <zach7505@gmail.com>
2024-07-24 23:14:55 +1000

    feat: added link to linkedin rather than resume for now

--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,10 @@ export default function Home() {
       <div id="home" className="p-4 lg:p-10 border-b border-black dark:border-white">
         <h1 className="dark:text-white text-4xl md:text-5xl lg:text-7xl pb-16 pt-16 text-center font-mono font-extrabold">Hey, I'm Zach!</h1>
         <h2 className="dark:text-white text-1xl md:text-3xl lg:text-4xl pb-10 text-center font-mono">I'm a <span className="text-primary-700 font-semibold">software developer</span>, with a speciality in <span className="text-accent-700 italic">quantum computing</span></h2>
-
         <div className="justify-center items-center flex mt-5 pb-20">
-          <button className="mx-2 px-4 py-1 lg:text-lg rounded-full border-2 border-black dark:border-white font-mono dark:text-white hover:bg-black hover:dark:bg-white hover:text-white hover:dark:text-slate-800">
+          <a href="https://www.linkedin.com/in/zachclare" target="_blank" rel="noopener noreferrer" className="mx-2 px-4 py-1 lg:text-lg rounded-full border-2 border-black dark:border-white font-mono dark:text-white hover:bg-black hover:dark:bg-white hover:text-white hover:dark:text-slate-800">
             Resume
-          </button>
+          </a>
         </div>
       </div>
 
