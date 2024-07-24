f1e762fa8893e13b79b40b691b04640e1355f0d8
Zekii <zach7505@gmail.com>
2024-07-24 22:16:26 +1000

    feat: added colour to project tags

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -4,17 +4,24 @@ import Image from 'next/image'
 function Tags({tags }: { tags: string[] }) {
     return (
         <div className='flex flex-wrap my-2'>
-            {tags.map((tag) => (
-                <span key={tag} className='text-xs font-semibold text-gray-700 dark:text-gray-200 font-mono border-gray-300 dark:border-slate-600 hover:bg-gray-300 dark:hover:bg-slate-600 rounded-full border px-2 py-1 mb-1 mr-1'>{tag}</span>
+            {tags.map((tag, index) => (
+                <span
+                    key={tag}
+                    className={`text-xs font-semibold  font-mono  border-gray-300 dark:border-slate-600  rounded-full border px-3 py-1 mb-1 mr-1 ${
+                        index === 0 ? 'bg-primary-700 border-none text-white dark:text-black hover:bg-primary-600' : 'text-gray-700 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-slate-600'
+                    }`}
+                >
+                    {tag}
+                </span>
             ))}
         </div>
-    )
+    );
 }
 
 export default function ProjectCard({ project }: { project: any }) {
     
     return (
-            <article className="flex overflow-hidden flex-col items-center border border-gray-200 hover:bg-gray-100 shadow dark:border-slate-700 dark:hover:bg-slate-700 m-2 rounded-lg">
+            <article className="flex overflow-hidden flex-col items-center border border-gray-200 shadow dark:border-slate-700  m-2 rounded-lg">
                 <Image
                     src={project.image}
                     width={544}
